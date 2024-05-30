e581acc16350c18e38bf4bf1249f6c5c8e78f35d
Milan Poudel <milanwebdeveloper1@gmail.com>
2024-05-30 10:19:08 +0545

    Modify user models struct

--- models/user.go
+++ models/user.go
@@ -1,8 +1,12 @@
 package models
 
 type User struct {
-	ID       uint   `json:"id"`
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	ID           uint   `json:"id"`
+	Name         string `json:"name"`
+	Email        string `json:"email"`
+	Password     string `json:"password"`
+	Country      string `json:"country"`
+	Role         string `json:"role"`
+	ProfileImage string `json:"profile_image"`
+	Verified     bool   `json:"verified"`
 }
